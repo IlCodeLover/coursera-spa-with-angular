006cd57702681ce79e420404218963b68abb0705
IlCodeLover <ilcodelover@gmail.com>
2025-05-17 10:46:46 +0200

    [setup] structure project and setup Angular.js, view (html) and app.js

---
+++ examples/Lecture04/app.js
@@ -0,0 +1,8 @@
+(function() {
+'use strict'; // to make sure we will not leak variables into global scope
+
+angular.module('myFirstApp', [])
+.controller('MyFirstController', function() { // this is a ViewModel
+
+});
+})();
