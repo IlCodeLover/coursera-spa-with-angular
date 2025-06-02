b225c3b67de19ac6c7addc718beee363690e63b0
IlCodeLover <ilcodelover@gmail.com>
2025-06-02 19:39:10 +0200

    [Lec04] basic angular with MVVM

--- examples/Lecture04/app.js
+++ examples/Lecture04/app.js
@@ -1,7 +1,7 @@
 (function() {
 'use strict'; // to make sure we will not leak variables into global scope
 
-angular.module('myFirstApp', [])
+angular.module('myFirstApp', []) // declare a module with dependency in []
 // this binds the ViewModel to the View
 .controller('MyFirstController', function($scope) { // this is a ViewModel
     $scope.name = "John Doe";
@@ -10,5 +10,5 @@ angular.module('myFirstApp', [])
     }
 
 
-});
+}); // end of controller
 })();
