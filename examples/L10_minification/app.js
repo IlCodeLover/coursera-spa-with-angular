6f093e3a408007557ce28561caaaf45c6669a0f9
IlCodeLover <ilcodelover@gmail.com>
2025-06-03 11:43:47 +0200

    [L10-minification] DI in Angular need to be minification proof

--- examples/L10_minification/app.js
+++ examples/L10_minification/app.js
@@ -2,6 +2,8 @@
     'use strict';
     angular.module('MinificationExample', [])
     .controller('MinificationController', MinificationController);
+    // declare the property $inject on the function MinificationController
+    MinificationController.$inject = ['$scope', '$filter'];
     function MinificationController($scope, $filter){
       $scope.name = "name";
       $scope.upper = function(){
@@ -9,4 +11,16 @@
         $scope.name = upperFilter($scope.name)
       }  
     }
-})();
+})(); 
+
+/*
+//  Version with minification error
+angular.module("MinificationExample",[]).controller("MinificationController",function n(e,i){e.name="name",e.upper=function(){var n=i("uppercase");e.name=n(e.name)}});
+*/
+
+/*
+//Solution 1:  working version with minification by adding the dependencies as an array
+angular.module("MinificationExample",[]).controller("MinificationController",["$scope","$filter",function n(e,i){e.name="name",e.upper=function(){var n=i("uppercase");e.name=n(e.name)}}]);
+*/
+// Solution 2: working version with minification by using the $injector service
+!function(){"use strict";function n(n,e){n.name="name",n.upper=function(){var i=e("uppercase");n.name=i(n.name)}}angular.module("MinificationExample",[]).controller("MinificationController",n),n.$inject=["$scope","$filter"]}();
